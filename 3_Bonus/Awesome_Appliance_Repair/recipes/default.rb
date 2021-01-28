70a9c1549857ae023ad46e12f5519860fc4408ef
Lakshman Annepu <lakshman.annepu@gmail.com>
2021-01-28 14:55:07 +0000

    updated latest recipe changes

---
+++ 3_Bonus/Awesome_Appliance_Repair/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: aar
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+include_recipe 'Awesome_Appliance_Repair::prerequisits'
---
+++ 3_Bonus/Awesome_Appliance_Repair/recipes/prerequisits.rb
@@ -0,0 +1,38 @@
+package "unzip" do
+  action :install
+end
+
+package "apache2" do
+  action :install
+end
+
+service "apache2" do
+  action [:enable, :start]
+end
+
+package "python" do
+  action :install
+end
+
+#mysql_service 'default' do
+#  port '3306'
+#  version '8.0'
+#  initial_root_password 'change me'
+#  action [:create, :start]
+#end
+
+git "download_app" do
+  repository 'git://github.com/chef-training/Awesome-Appliance-Repair.git'  
+  revision 'master'
+  action :sync
+end
+
+bash 'install_app' do
+code <<-EOH
+    cd /download_app
+	mv AAR /var/www
+	sudo su root
+	python AARInstall.py
+  EOH
+end
+
